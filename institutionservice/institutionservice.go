c95d4157c7f496365a8700605849dc2ff565f399
Wilder Pereira <wilder_roberto@hotmail.com>
2017-04-05 23:38:16 -0300

    Changed from xml to json

--- institutionservice/institutionservice.go
+++ institutionservice/institutionservice.go
@@ -13,8 +13,8 @@ func New() *restful.WebService {
     service := new(restful.WebService)
     service.
         Path("/institutions").
-        Consumes(restful.MIME_XML, restful.MIME_JSON).
-        Produces(restful.MIME_XML, restful.MIME_JSON)
+        Consumes(restful.MIME_JSON, restful.MIME_JSON).
+        Produces(restful.MIME_JSON, restful.MIME_JSON)
         
     service.Route(service.GET("/{institution-id}").To(FindInstitution))
     //service.Route(service.POST("").To(UpdateInstitution))
