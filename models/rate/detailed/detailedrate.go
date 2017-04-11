0022931c95fc50f3225542db73e50dc87db8f806
Wilder Pereira <wilder_roberto@hotmail.com>
2017-04-11 01:34:35 -0300

    Added detailed rate model

---
+++ models/rate/detailed/detailedrate.go
@@ -0,0 +1,40 @@
+package specific
+
+import (
+	"time"
+	"fiscaluno-ws/models/institution"
+)
+
+/*
+Id
+		Instituicao
+		Avaliacao (numerico das estrelas)
+		Data
+		Categoria
+		curso
+		idAluno
+ */
+
+type GeneralRate struct {
+	Id string
+	Description string
+	Category string
+	Rate float32
+	RatedBy string
+	Institution institution.Institution
+	//Course course.Course
+	RatedAt time.Time
+}
+
+// Institution constructor
+func New(id, desc, category, ratedBy string, rate float32, institution institution.Institution) *GeneralRate {
+	return &GeneralRate{
+		Id:id,
+		Description:desc,
+		Rate:rate,
+		Category: category,
+		RatedBy:ratedBy,
+		Institution: institution,
+		RatedAt: time.Now(),
+	}
+}
