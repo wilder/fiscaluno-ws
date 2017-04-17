c8a6ffb682762216f7cb6fdf917dfcffc52c2683
Wilder Pereira <wilder_roberto@hotmail.com>
2017-04-16 23:07:55 -0300

    Added dbcontract tests

--- playground.go
+++ playground.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"log"
-            "github.com/zabawaba99/firego"
-	"fiscaluno-ws/models/rate/general"
-            "fiscaluno-ws/models/institution"
-	"fiscaluno-ws/models/student"
-	//"encoding/json"
+	"github.com/zabawaba99/firego"
+	//"fiscaluno-ws/models/rate/general"
+	"fiscaluno-ws/models/institution"
+	//"fiscaluno-ws/models/student"
+	"fmt"
+	"fiscaluno-ws/database"
 )
 
 
@@ -24,30 +25,29 @@ func main() {
 
 	f := firego.New("https://***REMOVED***", nil)
 
-            institution := *institution.New( "f23fn9xsxx090nd3d", "Faculdade Impacta", 0.0)
+	/*institution := *institution.New("f23fn9xsxx090nd3d", "Faculdade Impacta", 0.0)
 	gRate := general.New("f134fqwefq4", "Muito Boaaa", "eu", 4.5, institution)
-            student := *student.New("xxxxYYYYzzzz", "Subaru", institution, *gRate)
+	student := *student.New("xxxxYYYYzzzz", "Subaru", institution, *gRate)*/
 
-	//gRateJson, err := json.Marshal(gRate)
-
-	pushedFirego, err := f.Push(gRate)
+	/*_, err := f.Push(gRate)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-            pushedStudent, err := f.Push(student)
-            if err != nil {
-                log.Fatal(err)
-            }
+	_, err = f.Push(student)
+	if err != nil {
+		log.Fatal(err)
+	}*/
 
-	var bar string
-	if err := pushedFirego.Value(&bar); err != nil {
+	var v map[string]interface{}
+	if err := f.Value(&v); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("%s\n", v)
 
-            var teste string
-            if err := pushedStudent.Value(&teste); err != nil {
-                log.Fatal(err)
-            }
+
+	//Testing if Firebase implements DBcontract
+	var db database.DBcontract = database.Firebase{}
+	db.Save(institution.New("a","a",4.4))
 
 }
