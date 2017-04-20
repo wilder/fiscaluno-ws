118a80764b1f7683fabdc59bab070a8d31663064
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-04-20 16:16:12 -0300

    Institution dao find institution by id retrieves institution model

--- dao/institutiondao/institutiondao.go
+++ dao/institutiondao/institutiondao.go
@@ -1,14 +1,22 @@
 package institutiondao
 
 import (
+    "strconv"
     "fiscaluno-ws/database"
     "fiscaluno-ws/database/filter"
-    //"fiscaluno-ws/models/institution"
+    "fiscaluno-ws/models/institution"
 )
 
 var db database.DBcontract = database.GetInstance()
 
-func FindInstitutionById (id string) interface{} {
-    var filterList = []filter.Filter{*filter.New("Id", id, "=")}
-    return db.Find("Institution", filterList)
+func FindInstitutionById (id string) *institution.Institution {
+    var filterList = [] filter.Filter{*filter.New("Id", id, "=")}
+    institution_node := db.Find("Institution", filterList).(map[string] interface{})
+    
+    for key := range institution_node {
+        institution_node = institution_node[key].(map[string] interface{})
+    }
+
+    institution_id := strconv.FormatFloat( institution_node["Id"].(float64), 'f', 1, 64 )
+    return  institution.New(  institution_id, institution_node["Name"].(string), float32( institution_node["Rate"].(float64) ) )
 }
