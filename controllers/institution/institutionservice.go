f21896799cc85b8f6cde3eec237ef62ab0b57046
Wilder Pereira <wilder_roberto@hotmail.com>
2017-04-23 02:23:35 -0300

    improved findInstitution

--- controllers/institution/institutionservice.go
+++ controllers/institution/institutionservice.go
@@ -1,20 +1,14 @@
 package institution
 
 import (
-    "strconv"
     "fiscaluno-ws/models/institution"
     "fiscaluno-ws/dao/institutiondao"
+    "fiscaluno-ws/utils"
 )
 
 func findInstitution(id string) *institution.Institution{
     institution_interface := institutiondao.FindInstitutionById(id)
-
-    //removing the firebase node
-    institution_node :=  institution_interface.(map[string] interface{})
-    for key := range institution_node {
-        institution_node = institution_node[key].(map[string] interface{})
-    }
-
-    institution_id := strconv.FormatFloat( institution_node["Id"].(float64), 'f', 1, 64 )
-    return institution.New( institution_id, institution_node["Name"].(string), float32( institution_node["Rate"].(float64) ) )
+    inst := &institution.Institution{}
+    utils.FirebaseToStruct(institution_interface, inst)
+    return inst
 }
