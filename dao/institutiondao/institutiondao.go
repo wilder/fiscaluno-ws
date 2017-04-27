973355a6ab8934bde3a1e525ba688c51fb51ab49
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-04-27 16:09:16 -0300

    CreateNewFunction implemented

--- dao/institutiondao/institutiondao.go
+++ dao/institutiondao/institutiondao.go
@@ -11,3 +11,7 @@ func FindInstitutionById (id string) interface {}{
     var filterList = [] filter.Filter{*filter.New("Id", id, "=")}
     return db.Find("Institution", filterList)
 }
+
+func CreateInstitution(institution interface{}) {
+    db.Save(institution)
+}
