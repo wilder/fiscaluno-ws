3f46703e9092bf3e5a9ac338278d7aa668c6396b
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-05-05 15:38:31 -0300

    db save can save object passing node id to firebase

--- database/firebase.go
+++ database/firebase.go
@@ -25,16 +25,22 @@ func GetInstance() firebase {
 
 //creates a new node on firebase of the object passed as argument
 //parameter: Any struct
-func (fb firebase) Save(object interface{}) {
+func (fb firebase) Save(object interface{}, ref ...string) {
 	nodeName := getType(object)
-	//TODO: improve
-	ref := firego.New("https://"+config.FirebaseUrl()+"/"+nodeName, nil)
+	var err error
 
-	_, err := ref.Push(object)
+	if( (len(ref) > 0) && (ref[0] != "") ){
+		ref := firego.New("https://"+config.FirebaseUrl()+"/"+nodeName+"/"+ref[0], nil)
+		err = ref.Set(object)
+	}else{
+		ref := firego.New("https://"+config.FirebaseUrl()+"/"+nodeName, nil)
+		_, err = ref.Push(object)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	
 	log.Print("saving...")
 	log.Print("saved new "+nodeName)
 }
